42d9f67ac322c8f1e77cc9d5525965680da00b09
Padang Perwira Yudha <terrificsubs@gmail.com>
2018-02-07 08:08:13 +0700

    1.0.0
    1.0.0

---
+++ yudha.SilihOtomatis.lua
@@ -0,0 +1,50 @@
+script_name="Silih Otomatis"
+script_description = "Melakukan aksi silih pada dialog."
+script_author      = "Padang Perwira Yudha"
+script_version     = "1.0.0"
+
+function silih(bintang, subs)
+	local pola = "{%"..bintang.."}([^{]*){%"..bintang.."([^%"..bintang.."]?[^}]*)}"
+	local silih = "{%"..bintang.."}%2{%"..bintang.."%1}"
+
+for i=1, #subs do
+	local dialog = subs[i]
+
+	if dialog.class == "dialogue" then
+
+			local style = dialog.style
+			local efek = dialog.effect
+			local proses = false
+
+	  if style:match("Default") then proses = true end
+      if style:match("Miring") then proses = true end
+      if style:match("Dialog") then proses = true end
+
+
+		 if proses then
+
+			dialog.text = dialog.text
+
+		   -- Mindah "{*}Padang-kun{*Mas Padang}" ke "{*}Mas Padang{*Padang-kun}"
+          :gsub(pola, silih)
+
+           end
+
+      subs[i] = dialog
+
+    end
+
+  end
+
+end
+
+function honorifics(subs, selected, active)
+  silih("*", subs)
+  aegisub.set_undo_point("Silih Honorifik")
+end
+
+aegisub.register_macro(
+  "Silih Otomatis",
+  "Memindahkan dialog dengan karakter asterisk (*)",
+  honorifics
+)
