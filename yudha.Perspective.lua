7ab3f7060c09263bac6dc9156a1f08054add3a0c
Padang Perwira Yudha <terrificsubs@gmail.com>
2018-02-12 06:26:54 +0700

    1.0.0

---
+++ yudha.Perspective.lua
@@ -0,0 +1,87 @@
+script_name="Perspective"
+script_description = "Script that finds .ass perspective for a known rectangle->tetragon projection"
+script_author      = "Padang Perwira Yudha"
+script_version     = "1.0.0"
+script_namespace="yudha.Perspective"
+
+local haveDepCtrl,DependencyControl,depRec=pcall(require,"l0.DependencyControl")
+if haveDepCtrl then
+   script_version="1.0.0"
+    depRec=DependencyControl{feed="https://raw.githubusercontent.com/Pcpkomputer/.bulan/master/DependencyControl.json"}
+end
+
+
+function main(subs, sel)
+		ADD=aegisub.dialog.display
+		ADP=aegisub.decode_path
+		ADO=aegisub.dialog.open
+		scriptpath=ADP("?script").."\\"
+		vpath="D:\\"
+		kosong=""
+		for x, i in ipairs(sel) do
+		line=subs[i]
+		text=line.text
+		if x==1 then
+		tags=text:match("%([^%)]-%)")
+		end
+		hasil=tags
+		:gsub("%(","<")
+		:gsub("%)",">")
+ADD=aegisub.dialog.display
+GUI=
+{
+	{x=0,y=0,width=1,height=1,class="label",label="Versi 1.0.0",},
+    {x=0,y=1,width=1,height=1,class="label",label="Koordinat :",},
+    {x=1,y=1,width=1,height=1,class="edit",name="coor", value=""..hasil.."",},
+	{x=0,y=2,width=1,height=1,class="label",label="Rasio :",},
+    {x=1,y=2,width=1,height=1,class="edit",name="ras",value="3.5",},
+	{x=0,y=3,width=1,height=1,class="checkbox", name="cekorigin",label="Origin :",},
+	{x=1,y=3,width=1,height=1,class="edit",name="orz",},
+	{x=0,y=4,width=1,height=1,class="checkbox", name="cekscale",label="Scale :",},
+	{x=1,y=4,width=1,height=1,class="edit",name="scl",},
+}
+repeat
+P,res=ADD(GUI,
+{"Proses","Kembali"},{ok='Proses',close='Kembali'})
+until P=="Proses" or P=="Kembali"
+--
+
+if res["cekscale"] then
+skrup="perspective.py \""..res.coor.."\" -r "..res.ras.." -s "..res.scl.."\n@pause"
+else
+skrup="perspective.py \""..res.coor.."\" -r "..res.ras.."\n@pause"
+end
+if res["cekorigin"] then
+skrup="perspective.py \""..res.coor.."\" -r "..res.ras.." -o "..res.orz.."\n@pause" end
+if res["cekorigin"] and res["cekscale"] then
+skrup="perspective.py \""..res.coor.."\" -r "..res.ras.." -o "..res.orz.." -s "..res.ras.."\n@pause" end
+
+--
+if P=="Kembali" then    aegisub.cancel() end
+if hasil==nil or hasil=="" or tags==nil then t_error("Enggak ada koordinatnya goblok!",1) end
+end
+
+crot=vpath.."temp.bat"
+
+local pl0x=io.open(vpath.."temp.bat","w")
+pl0x:write(skrup)
+pl0x:close()
+-----
+aegisub.progress.title("Memproses...")
+crot=crot:gsub("%=","^=")
+os.execute(crot)
+-----
+function t_error(message,cancel)
+ADD=aegisub.dialog.display
+ADD({{class="label",label=message}},{"OK"},{close='OK'})
+if cancel then aegisub.cancel() end
+end
+-----
+function quo(x) x="\""..x.."\"" return x end
+-----
+aegisub.set_undo_point(script_name)
+end
+-----
+
+if haveDepCtrl then depRec:registerMacro(main) else aegisub.register_macro(script_name,script_description,main) end
+
